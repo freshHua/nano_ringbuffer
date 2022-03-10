7a11bf3c946ce50a88ffedbe6452bd018fcceaeb
Junbo Zheng <zhengjunbo1@xiaomi.com>
2022-03-10 11:27:17 +0800

    ringbuffer_test.c: replace fix len array with unspecified fixed len to avoid overflow
    Signed-off-by: Junbo Zheng <zhengjunbo1@xiaomi.com>

--- ringbuffer_test.c
+++ ringbuffer_test.c
@@ -9,20 +9,24 @@
 
 struct nano_ringbuffer_t s_ringbuffer;
 
-int main(int argc, char*argv[]){
-	unsigned char frame[8] = {"12345678"};
-	unsigned char buffer[16];
-	unsigned int len;
+int main(int argc, char*argv[])
+{
+	unsigned char frame[] = {"12345678"};
+	unsigned char buffer[16] = {0};
+	unsigned int len = sizeof(buffer);
 
 	printf("Nano ringbuffer testing\n") ;
 
-	nano_ringbuffer_init(&s_ringbuffer,16);
+	nano_ringbuffer_init(&s_ringbuffer, len);
+
 	nano_ringbuffer_put(&s_ringbuffer, frame,sizeof(frame));
-	len = sizeof(buffer);
 	printf("Ring buffer put %d used %d\n", len, nano_ringbuffer_len(&s_ringbuffer));
-	if( nano_ringbuffer_get(&s_ringbuffer, buffer, &len) >= 0){
+
+	if(nano_ringbuffer_get(&s_ringbuffer, buffer, &len) >= 0)
+	{
 		buffer[len] = '\0';
 		printf("Ring buffer get %d buffer %s\n", len, buffer);
 	}
+
 	return 0 ;
 }
